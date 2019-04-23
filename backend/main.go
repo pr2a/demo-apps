9e4fc50a719fb3c4e491e09f3719a67b422246a4
Rongjian Lan <rongjian.lan@gmail.com>
2019-04-22 20:03:04 -0700

    Add appengine setup and email sending

--- backend/main.go
+++ backend/main.go
@@ -4,12 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"path"
 	"time"
 
 	"github.com/harmony-one/demo-apps/backend/client"
 	"github.com/harmony-one/demo-apps/backend/db"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/mail"
 )
 
 var (
@@ -26,7 +29,7 @@ func printVersion(me string) {
 
 var (
 	collection = flag.String("collection", "players", "name of collection")
-	key        = flag.String("key", "./keys/benchmark_account_key.json", "key filename")
+	key        = flag.String("key", "./keys/leo_account_key.json", "key filename")
 	project    = flag.String("project", "lottery-demo-leo", "project ID of firebase")
 	ip         = flag.String("server_ip", "34.222.210.98", "the IP address of the server")
 
@@ -38,6 +41,31 @@ const (
 )
 
 func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/pickwinner", pickWinnerHandler)
+
+	appengine.Main()
+}
+
+// indexHandler responds to requests with our greeting.
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello, World!")
+}
+
+// pickWinnerHandler responds to requests for the pick winner cron job.
+func pickWinnerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/pickwinner" {
+		http.NotFound(w, r)
+		return
+	}
+	pickWinner(w, r)
+}
+
+func pickWinner(w http.ResponseWriter, r *http.Request) {
 	flag.Parse()
 	if *versionFlag {
 		printVersion(os.Args[0])
@@ -59,14 +87,14 @@ func main() {
 	if err != nil {
 		log.Fatalf("GetPlayer Error: %v", err)
 	} else {
-		fmt.Printf("Player: %v\n", player)
+		log.Printf("Player: %v\n", player)
 	}
 
 	winner, err := restclient.GetWinner(*ip, port)
 	if err != nil {
 		log.Fatalf("GetWinner Error: %v", err)
 	} else {
-		fmt.Printf("Winner: %v\n", winner)
+		log.Printf("Winner: %v\n", winner)
 	}
 
 	// wait for the execution of smart contracts
@@ -76,8 +104,32 @@ func main() {
 	if err != nil {
 		log.Fatalf("GetPlayer Error: %v", err)
 	} else {
-		fmt.Printf("Player: %v\n", player)
+		log.Printf("Player: %v\n", player)
 	}
 
-	os.Exit(0)
+	keys, ok := r.URL.Query()["email"]
+	email := "rongjian@harmony.one"
+	if !ok || len(keys[0]) < 1 {
+		log.Println("Url Param 'email' is missing")
+	} else {
+		email = keys[0]
+	}
+
+	sendEmail([]string{email}, r)
+
+	fmt.Fprint(w, "Winner email sent!")
+}
+
+func sendEmail(recipients []string, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	msg := &mail.Message{
+		Sender:  "admin@harmony-lottery-app.appspotmail.com",
+		To:      recipients,
+		Subject: "Lottery Winner is Picked!!!",
+		Body:    "You are the winner!!!",
+	}
+	log.Printf("Sending email: %s", msg)
+	if err := mail.Send(ctx, msg); err != nil {
+		log.Println(ctx, "Couldn't send email", err)
+	}
 }
